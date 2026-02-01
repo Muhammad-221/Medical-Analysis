54c5c35d7cf783f37f5ea862a79aeeec84cd8fc0
Muhamad Abdelmawla <mohamedelkalb221@gmail.com>
2026-02-01 13:44:06 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { createRoot } from 'react-dom/client'
 // import "bootstrap/dist/css/bootstrap.css";
 import './App.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Medical-Analysis">
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 )
