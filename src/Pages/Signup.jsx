f588fe5e922261d146e4179fe2913c2abd6e68a4
Muhamad Abdelmawla <mohamedelkalb221@gmail.com>
2026-05-05 01:18:19 +0300

    updated

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,9 +1,9 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { useAuth } from '@/contexts/AuthContext';
 import { useState } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
-import { Label } from './ui/label';
-import { Input } from './ui/input';
-import { Button } from './ui/button';
 import { toast } from 'sonner';
 
 function Signup() {
