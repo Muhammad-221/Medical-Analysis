752784b43c5208d644699ce83a52711be82246db
Muhamad Abdelmawla <mohamedelkalb221@gmail.com>
2026-05-11 11:37:37 +0300

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.jsx'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import AuthProvider from './contexts/AuthContext'
 import { Toaster } from "sonner";
@@ -9,10 +9,10 @@ import { Toaster } from "sonner";
 createRoot(document.getElementById('root')).render(
   <ThemeProvider>
     <AuthProvider>
-      <HashRouter>
+      <BrowserRouter> 
         <Toaster position="top-right" richColors/> 
         <App />
-      </HashRouter>
+      </BrowserRouter> 
     </AuthProvider>
   </ThemeProvider>
 )
