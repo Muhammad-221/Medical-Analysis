fe322284cbab649fc493e8349add14ccca9fd731
Muhamad Abdelmawla <mohamedelkalb221@gmail.com>
2026-05-11 11:02:25 +0300

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import AuthProvider from './contexts/AuthContext'
 import { Toaster } from "sonner";
@@ -9,10 +9,12 @@ import { Toaster } from "sonner";
 createRoot(document.getElementById('root')).render(
   <ThemeProvider>
     <AuthProvider>
-      <BrowserRouter basename="/Medical-Analysis/"> 
-        <Toaster position="top-right" richColors/> 
-        <App />
-      </BrowserRouter> 
+      <HashRouter>
+        <BrowserRouter basename="/Medical-Analysis/"> 
+          <Toaster position="top-right" richColors/> 
+          <App />
+        </BrowserRouter> 
+      </HashRouter>
     </AuthProvider>
   </ThemeProvider>
 )
