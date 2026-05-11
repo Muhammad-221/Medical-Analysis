dd08e891746ec085d10d4fcfacffd60a885868a7
Muhamad Abdelmawla <mohamedelkalb221@gmail.com>
2026-05-11 11:41:13 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  base: "/Medical-Analysis/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
